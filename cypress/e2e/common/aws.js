507b4aad04204cf16208445b6a2964482bfea2e9
RichardDKlein <RichardDKlein@gmail.com>
2024-07-24 09:51:23 -0500

    Refactor startup logic

---
+++ cypress/e2e/common/aws.js
@@ -0,0 +1,67 @@
+/// <reference types="cypress" />
+
+import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
+
+export function getAwsRegion() {
+  const awsConfigFilePath = `${Cypress.env("HOME")}/.aws/config`;
+  return cy.task("loadAwsConfig", awsConfigFilePath).then((awsConfig) => {
+    const region = awsConfig.default.region;
+    return region;
+  });
+}
+
+export function createSsmClient(region) {
+  const awsCredentialsFilePath = `${Cypress.env("HOME")}/.aws/credentials`;
+  return cy
+    .task("loadAwsCredentials", awsCredentialsFilePath)
+    .then((awsCredentials) => {
+      const ssmClient = new SSMClient({
+        region,
+        credentials: {
+          accessKeyId: awsCredentials.default.aws_access_key_id,
+          secretAccessKey: awsCredentials.default.aws_secret_access_key,
+        },
+      });
+      return ssmClient;
+    });
+}
+
+export function getAdminUsername(ssmClient) {
+  const command = new GetParameterCommand({
+    Name: "/shortUrl/users/adminUsername",
+    WithDecryption: true,
+  });
+  return cy.wrap(ssmClient.send(command)).then((response) => {
+    return response.Parameter.Value;
+  });
+}
+
+export function getAdminPassword(ssmClient) {
+  const command = new GetParameterCommand({
+    Name: "/shortUrl/users/adminPassword",
+    WithDecryption: true,
+  });
+  return cy.wrap(ssmClient.send(command)).then((response) => {
+    return response.Parameter.Value;
+  });
+}
+
+export function getJwtSecretKey(ssmClient) {
+  const command = new GetParameterCommand({
+    Name: "/shortUrl/users/jwtSecretKey",
+    WithDecryption: true,
+  });
+  return cy.wrap(ssmClient.send(command)).then((response) => {
+    return response.Parameter.Value;
+  });
+}
+
+export function getJwtMinutesToLive(ssmClient) {
+  const command = new GetParameterCommand({
+    Name: "/shortUrl/users/jwtMinutesToLive",
+    WithDecryption: true,
+  });
+  return cy.wrap(ssmClient.send(command)).then((response) => {
+    return response.Parameter.Value;
+  });
+}
--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,70 +1,31 @@
-import "./commands";
-import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
+import {
+  createSsmClient,
+  getAdminUsername,
+  getAdminPassword,
+  getAwsRegion,
+  getJwtSecretKey,
+  getJwtMinutesToLive,
+} from "../e2e/common/aws";
+import { GetParameterCommand } from "@aws-sdk/client-ssm";
 
 // Do the following logic before running any tests.
 
 before(() => {
-  // Fetch the AWS region from the AWS config file.
-  const awsConfigFilePath = `${Cypress.env("HOME")}/.aws/config`;
-  cy.task("loadAwsConfig", awsConfigFilePath).then((awsConfig) => {
-    const region = awsConfig.default.region;
+  getAwsRegion().then((region) => {
     Cypress.env("region", region);
-
-    // Fetch the AWS credentials from the AWS credentials file.
-    const awsCredentialsFilePath = `${Cypress.env("HOME")}/.aws/credentials`;
-    cy.task("loadAwsCredentials", awsCredentialsFilePath).then(
-      (awsCredentials) => {
-        // Create an AWS SSM client.
-        const ssmClient = new SSMClient({
-          region,
-          credentials: {
-            accessKeyId: awsCredentials.default.aws_access_key_id,
-            secretAccessKey: awsCredentials.default.aws_secret_access_key,
-          },
-        });
-
-        // Fetch the admin username from the Parameter Store.
-        const command = new GetParameterCommand({
-          Name: "/shortUrl/users/adminUsername",
-          WithDecryption: true,
-        });
-        ssmClient.send(command).then((response) => {
-          const adminUsername = response.Parameter.Value;
-          Cypress.env("adminUsername", adminUsername);
-
-          // Fetch the admin password from the Parameter Store.
-          const command = new GetParameterCommand({
-            Name: "/shortUrl/users/adminPassword",
-            WithDecryption: true,
-          });
-          ssmClient.send(command).then((response) => {
-            const adminPassword = response.Parameter.Value;
-            Cypress.env("adminPassword", adminPassword);
-
-            // Fetch the JWT secret key from the Parameter Store.
-            const command = new GetParameterCommand({
-              Name: "/shortUrl/users/jwtSecretKey",
-              WithDecryption: true,
-            });
-            ssmClient.send(command).then((response) => {
-              const jwtSecretKey = response.Parameter.Value;
-              Cypress.env("jwtSecretKey", jwtSecretKey);
-
-              // Fetch the JWT minutes to live from the Parameter Store.
-              const command = new GetParameterCommand({
-                Name: "/shortUrl/users/jwtMinutesToLive",
-                WithDecryption: true,
-              });
-              ssmClient.send(command).then((response) => {
-                const jwtMinuesToLive = response.Parameter.Value;
-                Cypress.env("jwtMinutesToLive", jwtMinuesToLive);
-              });
+    createSsmClient(region).then((ssmClient) => {
+      getAdminUsername(ssmClient).then((adminUsername) => {
+        Cypress.env("adminUsername", adminUsername);
+        getAdminPassword(ssmClient).then((adminPassword) => {
+          Cypress.env("adminPassword", adminPassword);
+          getJwtSecretKey(ssmClient).then((jwtSecretKey) => {
+            Cypress.env("jwtSecretKey", jwtSecretKey);
+            getJwtMinutesToLive(ssmClient).then((jwtMinuesToLive) => {
+              Cypress.env("jwtMinutesToLive", jwtMinuesToLive);
             });
           });
         });
-      }
-    );
+      });
+    });
   });
-  // Give the `Cypress.env()` writes some time to settle.
-  cy.wait(1000);
 });
