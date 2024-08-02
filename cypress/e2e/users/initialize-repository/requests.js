19b717a5d534bd8c274b0e38693b177922666a6f
RichardDKlein <RichardDKlein@gmail.com>
2024-08-02 15:32:12 -0500

    db-init => initialize-repository

--- cypress/e2e/users/initialize-repository/requests.js
+++ cypress/e2e/users/initialize-repository/requests.js
@@ -5,7 +5,7 @@ import { USERS_BASE_URL } from "../../common/constants";
 export function initializeRepositoryWithNoAuthHeader() {
   return cy.request({
     method: "POST",
-    url: `${USERS_BASE_URL}/db-init`,
+    url: `${USERS_BASE_URL}/initialize-repository`,
     failOnStatusCode: false,
   });
 }
@@ -13,7 +13,7 @@ export function initializeRepositoryWithNoAuthHeader() {
 export function initializeRepositoryWithWrongKindOfAuthHeader() {
   return cy.request({
     method: "POST",
-    url: `${USERS_BASE_URL}/db-init`,
+    url: `${USERS_BASE_URL}/initialize-repository`,
     headers: {
       Authorization: "Bearer " + "dummy.jwt.token",
     },
@@ -38,7 +38,7 @@ export function initializeRepositoryWithValidAdminCredentials() {
 export function initializeRepositoryWithBasicAuthHeader(username, password) {
   return cy.request({
     method: "POST",
-    url: `${USERS_BASE_URL}/db-init`,
+    url: `${USERS_BASE_URL}/initialize-repository`,
     headers: {
       Authorization: "Basic " + btoa(`${username}:${password}`),
     },
